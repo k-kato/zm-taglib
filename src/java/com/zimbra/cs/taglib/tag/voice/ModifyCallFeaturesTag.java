799e0e0a477b266e40011ce4d1fae472bd4963ee
Unknown Perforce User <lars>
2009-10-22 04:53:02 -0500

    bug: 41934
    Enabled the selective call rejection interface to SOAP.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=41934
    
    Copied from Perforce
     Change: 215688

--- src/java/com/zimbra/cs/taglib/tag/voice/ModifyCallFeaturesTag.java
+++ src/java/com/zimbra/cs/taglib/tag/voice/ModifyCallFeaturesTag.java
@@ -45,24 +45,24 @@ public class ModifyCallFeaturesTag extends CallFeaturesTagBase {
             ZCallFeaturesBean newFeatures = new ZCallFeaturesBean(new ZCallFeatures(mailbox, account.getPhone()), true);
 	    
             ZVoiceMailPrefsBean voiceMailPrefs = oldFeatures.getVoiceMailPrefs();
-	    ZCallForwardingBean callForwardingNoAnswer = oldFeatures.getCallForwardingNoAnswer();
+	        ZCallForwardingBean callForwardingNoAnswer = oldFeatures.getCallForwardingNoAnswer();
 	    
             if (mEmailNotificationActive!=null && mEmailNotificationAddress!=null && 
-		((!mEmailNotificationActive.booleanValue() && 
-		(voiceMailPrefs.getEmailNotificationAddress() != null && voiceMailPrefs.getEmailNotificationAddress().length() > 0)) || !voiceMailPrefs.getEmailNotificationAddress().equalsIgnoreCase(mEmailNotificationAddress))) {
+		        ((!mEmailNotificationActive.booleanValue() && 
+		        (voiceMailPrefs.getEmailNotificationAddress() != null && voiceMailPrefs.getEmailNotificationAddress().length() > 0)) || !voiceMailPrefs.getEmailNotificationAddress().equalsIgnoreCase(mEmailNotificationAddress))) {
                 String address = mEmailNotificationActive.booleanValue() ? mEmailNotificationAddress : "";
                 newFeatures.getVoiceMailPrefs().setEmailNotificationAddress(address);
             }
-	    
-	    ZCallForwardingBean callForwarding = oldFeatures.getCallForwardingAll();
+	        
+	        ZCallForwardingBean callForwarding = oldFeatures.getCallForwardingAll();
             if (mCallForwardingActive!=null && mCallForwardingForwardTo!=null && 
-		(callForwarding.getIsActive() != mCallForwardingActive.booleanValue() || !callForwarding.getForwardTo().equals(mCallForwardingForwardTo))) {
+	            (callForwarding.getIsActive() != mCallForwardingActive.booleanValue() || !callForwarding.getForwardTo().equals(mCallForwardingForwardTo))) {
                 ZCallForwardingBean newCallForwarding = newFeatures.getCallForwardingAll();
                 newCallForwarding.setIsActive(mCallForwardingActive.booleanValue());
                 newCallForwarding.setForwardTo(mCallForwardingForwardTo);
             }
-	    
-	    ZSelectiveCallForwardingBean selectiveCallForwarding = oldFeatures.getSelectiveCallForwarding();
+	        
+	        ZSelectiveCallForwardingBean selectiveCallForwarding = oldFeatures.getSelectiveCallForwarding();
 
             if (mSelectiveCallForwardingActive!=null) {
                 ZSelectiveCallForwardingBean newSelectiveCallForwarding = newFeatures.getSelectiveCallForwarding();
@@ -81,19 +81,17 @@ public class ModifyCallFeaturesTag extends CallFeaturesTagBase {
                         newSelectiveCallForwarding.setIsActive(false);
                     }
                 }
-	    }
-	    
-	    ZCallFeatureBean anonymousCallRejection = oldFeatures.getAnonymousCallRejection();
-	    if (mAnonymousCallRejectionActive!=null && 
-		(anonymousCallRejection.getIsActive() != mAnonymousCallRejectionActive.booleanValue())) {
-		newFeatures.getAnonymousCallRejection().setIsActive(mAnonymousCallRejectionActive.booleanValue());
-	    }
-	    
-	    /* Uncomment when selective call rejection is implemented in the soap interface
-	    
-	    ZSelectiveCallRejectionBean selectiveCallRejection = oldFeatures.getSelectiveCallRejection();
-	    if (mSelectiveCallRejectionActive!=null && mSelectiveCallRejectionRejectFrom!=null &&
-                (selectiveCallRejection.getIsActive() != mSelectiveCallRejectionActive.booleanValue() || !selectiveCallRejection.getRejectFrom().equals(mSelectiveCallRejectionRejectFrom))) {
+	        }
+	        
+	        ZCallFeatureBean anonymousCallRejection = oldFeatures.getAnonymousCallRejection();
+	        if (mAnonymousCallRejectionActive!=null && 
+		        (anonymousCallRejection.getIsActive() != mAnonymousCallRejectionActive.booleanValue())) {
+		        newFeatures.getAnonymousCallRejection().setIsActive(mAnonymousCallRejectionActive.booleanValue());
+	        }
+	        
+	        ZSelectiveCallRejectionBean selectiveCallRejection = oldFeatures.getSelectiveCallRejection();
+	        if (mSelectiveCallRejectionActive!=null && mSelectiveCallRejectionRejectFrom!=null &&
+                (selectiveCallRejection.getIsActive() != mSelectiveCallRejectionActive.booleanValue() || (selectiveCallRejection.getRejectFrom() != null && !selectiveCallRejection.getRejectFrom().equals(mSelectiveCallRejectionRejectFrom)))) {
                 ZSelectiveCallRejectionBean newSelectiveCallRejection = newFeatures.getSelectiveCallRejection();
                 newSelectiveCallRejection.setIsActive(mSelectiveCallRejectionActive.booleanValue());
                 if (mSelectiveCallRejectionActive.booleanValue()) {
@@ -103,48 +101,48 @@ public class ModifyCallFeaturesTag extends CallFeaturesTagBase {
                         newSelectiveCallRejection.setIsActive(false);
                     }
                 }
-	    }
-	    
-	    */
-	    
-	    if (mNumberOfRings != null && (callForwardingNoAnswer == null || callForwardingNoAnswer.getNumberOfRings() != mNumberOfRings.intValue())) {
-		newFeatures.getCallForwardingNoAnswer().setNumberOfRings(mNumberOfRings.intValue());
-	    }	
-	    if (mAutoPlayNewMsgs != null && (voiceMailPrefs == null || voiceMailPrefs.getAutoPlayNewMsgs() != mAutoPlayNewMsgs.booleanValue())) {
-		newFeatures.getVoiceMailPrefs().setAutoPlayNewMsgs(mAutoPlayNewMsgs.booleanValue());
-	    }
-	    if (mPlayDateAndTimeInMsgEnv != null && (voiceMailPrefs == null || voiceMailPrefs.getPlayDateAndTimeInMsgEnv() != mPlayDateAndTimeInMsgEnv.booleanValue())) {
-		newFeatures.getVoiceMailPrefs().setPlayDateAndTimeInMsgEnv(mPlayDateAndTimeInMsgEnv.booleanValue());
-	    }
-	    if (mSkipPinEntry != null && (voiceMailPrefs == null || voiceMailPrefs.getSkipPinEntry() != mSkipPinEntry.booleanValue())) {
-		newFeatures.getVoiceMailPrefs().setSkipPinEntry(mSkipPinEntry.booleanValue());
-	    }
-	    if (mPlayCallerNameInMsgEnv != null && (voiceMailPrefs == null || voiceMailPrefs.getPlayCallerNameInMsgEnv() != mPlayCallerNameInMsgEnv.booleanValue())) {
-		newFeatures.getVoiceMailPrefs().setPlayCallerNameInMsgEnv(mPlayCallerNameInMsgEnv.booleanValue());
-	    }
-	    if (mPromptLevel != null && mAutoPlayNewMsgs != null && mAutoPlayNewMsgs.booleanValue() && (voiceMailPrefs == null || voiceMailPrefs.getPromptLevel() == null || !voiceMailPrefs.getPromptLevel().equals(mPromptLevel))) {
-		newFeatures.getVoiceMailPrefs().setPromptLevel(mPromptLevel);
-	    }
-	    if (mAnsweringLocale != null && (voiceMailPrefs == null || voiceMailPrefs.getAnsweringLocale() == null || !voiceMailPrefs.getAnsweringLocale().equals(mAnsweringLocale))) {
-	        newFeatures.getVoiceMailPrefs().setAnsweringLocale(mAnsweringLocale);
-	    }
-	    if (mUserLocale != null && (voiceMailPrefs == null || voiceMailPrefs.getUserLocale() == null || !voiceMailPrefs.getUserLocale().equals(mUserLocale))) {
-	        newFeatures.getVoiceMailPrefs().setUserLocale(mUserLocale);
-	    }
-            
-	    boolean update = false;
+	        }
+	        
+	        
+	        
+	        if (mNumberOfRings != null && (callForwardingNoAnswer == null || callForwardingNoAnswer.getNumberOfRings() != mNumberOfRings.intValue())) {
+		        newFeatures.getCallForwardingNoAnswer().setNumberOfRings(mNumberOfRings.intValue());
+	        }	
+	        if (mAutoPlayNewMsgs != null && (voiceMailPrefs == null || voiceMailPrefs.getAutoPlayNewMsgs() != mAutoPlayNewMsgs.booleanValue())) {
+		        newFeatures.getVoiceMailPrefs().setAutoPlayNewMsgs(mAutoPlayNewMsgs.booleanValue());
+	        }
+	        if (mPlayDateAndTimeInMsgEnv != null && (voiceMailPrefs == null || voiceMailPrefs.getPlayDateAndTimeInMsgEnv() != mPlayDateAndTimeInMsgEnv.booleanValue())) {
+		        newFeatures.getVoiceMailPrefs().setPlayDateAndTimeInMsgEnv(mPlayDateAndTimeInMsgEnv.booleanValue());
+	        }
+	        if (mSkipPinEntry != null && (voiceMailPrefs == null || voiceMailPrefs.getSkipPinEntry() != mSkipPinEntry.booleanValue())) {
+		        newFeatures.getVoiceMailPrefs().setSkipPinEntry(mSkipPinEntry.booleanValue());
+	        }
+	        if (mPlayCallerNameInMsgEnv != null && (voiceMailPrefs == null || voiceMailPrefs.getPlayCallerNameInMsgEnv() != mPlayCallerNameInMsgEnv.booleanValue())) {
+		        newFeatures.getVoiceMailPrefs().setPlayCallerNameInMsgEnv(mPlayCallerNameInMsgEnv.booleanValue());
+	        }
+	        if (mPromptLevel != null && mAutoPlayNewMsgs != null && mAutoPlayNewMsgs.booleanValue() && (voiceMailPrefs == null || voiceMailPrefs.getPromptLevel() == null || !voiceMailPrefs.getPromptLevel().equals(mPromptLevel))) {
+		        newFeatures.getVoiceMailPrefs().setPromptLevel(mPromptLevel);
+	        }
+	        if (mAnsweringLocale != null && (voiceMailPrefs == null || voiceMailPrefs.getAnsweringLocale() == null || !voiceMailPrefs.getAnsweringLocale().equals(mAnsweringLocale))) {
+	            newFeatures.getVoiceMailPrefs().setAnsweringLocale(mAnsweringLocale);
+	        }
+	        if (mUserLocale != null && (voiceMailPrefs == null || voiceMailPrefs.getUserLocale() == null || !voiceMailPrefs.getUserLocale().equals(mUserLocale))) {
+	            newFeatures.getVoiceMailPrefs().setUserLocale(mUserLocale);
+	        }
+                
+	        boolean update = false;
             if (!newFeatures.isEmpty()) {
             	mailbox.saveCallFeatures(newFeatures.getCallFeatures());
-		update = true;
-	    }
-	    if (mNumberPerPage!=null && mailbox.getPrefs().getVoiceItemsPerPage() != mNumberPerPage.longValue()) {
-		Map<String, Object> attrs = new HashMap<String,Object>();
-		attrs.put(Provisioning.A_zimbraPrefVoiceItemsPerPage, Long.toString(mNumberPerPage.longValue()));
-		mailbox.modifyPrefs(attrs);
-		update = true;
-		mailbox.getPrefs(true);
-	    }
-	    getJspContext().setAttribute(mVar, update, PageContext.PAGE_SCOPE);
+	            update = true;
+            }
+	        if (mNumberPerPage!=null && mailbox.getPrefs().getVoiceItemsPerPage() != mNumberPerPage.longValue()) {
+		        Map<String, Object> attrs = new HashMap<String,Object>();
+		        attrs.put(Provisioning.A_zimbraPrefVoiceItemsPerPage, Long.toString(mNumberPerPage.longValue()));
+		        mailbox.modifyPrefs(attrs);
+		        update = true;
+		        mailbox.getPrefs(true);
+	        }
+	        getJspContext().setAttribute(mVar, update, PageContext.PAGE_SCOPE);
         } catch (ServiceException e) {
             throw new JspTagException(e);
         }
