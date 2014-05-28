d1e69dfe6f127dd50ca413681a2599c075d50ded
Greg Solovyev <greg@zimbra.com>
2014-05-28 12:56:09 -0500

    bug 83550  - move auth token registration from public method to contructor. This makes sure that all tokens are registered and reduces the amount of code changes  - cleanup expired tokens before registering a new one  - do not let ZimbraAuthToken encode 0 as expiration time since we do not allow ZimbraAuthToken to have 0 expiration time  - add checks for token registration everywhere where we check for token expiration  - add a call to EndSession SOAP handler to LogOut tag to make sure that the account will get logged out from SOAP when it is logged out from HTML client  - add tests for exploit scenarios
    http://bugzilla.zimbra.com/show_bug.cgi?id=83550
    
    Copied from Perforce
     Change: 512747

--- src/java/com/zimbra/cs/taglib/tag/LogoutTag.java
+++ src/java/com/zimbra/cs/taglib/tag/LogoutTag.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2006, 2007, 2008, 2009, 2010, 2013 Zimbra Software, LLC.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.4 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -22,14 +22,19 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspTagException;
 import javax.servlet.jsp.PageContext;
 
+import com.zimbra.client.ZMailbox;
 import com.zimbra.common.auth.ZAuthToken;
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.AuthToken;
 import com.zimbra.cs.account.AuthTokenException;
 import com.zimbra.cs.account.ZimbraAuthToken;
+import com.zimbra.cs.httpclient.URLUtil;
 import com.zimbra.cs.taglib.ZJspSession;
 
 public class LogoutTag extends ZimbraSimpleTag {
-    
+
+    @Override
     public void doTag() throws JspException, IOException {
         JspContext jctxt = getJspContext();
         PageContext pageContext = (PageContext) jctxt;
@@ -37,14 +42,19 @@ public class LogoutTag extends ZimbraSimpleTag {
         try {
         	if(authToken != null) {
         		AuthToken at = ZimbraAuthToken.getAuthToken(authToken.getValue());
+        		Account acc = at.getAccount();
         		if(at != null) {
-        			at.deRegister();
+        		    if(acc != null) {
+        		        ZMailbox mbox = ZMailbox.getByAuthToken(authToken, URLUtil.getSoapURL(acc.getServer(), false));
+        		        mbox.logout();//this invalidates the session in mailbox app
+        		    }
+        			at.deRegister(); //this invalidates the session in webclient app
         		}
         	}
-        } catch (AuthTokenException e) {
+        } catch (AuthTokenException | ServiceException e) {
 			throw new JspTagException(e.getMessage(), e);
 		} finally {
-	        HttpServletResponse response = (HttpServletResponse) pageContext.getResponse(); 
+	        HttpServletResponse response = (HttpServletResponse) pageContext.getResponse();
 	        ZAuthToken.clearCookies(response);
 	        ZJspSession.clearSession(pageContext);
 		}
