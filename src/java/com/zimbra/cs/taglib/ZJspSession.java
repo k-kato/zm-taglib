264eca58b431d12fc29f9f3d491f7468cbc6c27b
Vishal Mahajan <vmahajan@zimbra.com>
2014-07-04 03:38:50 -0500

    92295 - Modify Ajax Client to handle CSRF token
    Allow AuthRequest to happen while creating ZMailbox for serving rest requests, so that csrf token can be obtained from the server corresponding to the auth token.
    
    Copied from Perforce
     Change: 516545

--- src/java/com/zimbra/cs/taglib/ZJspSession.java
+++ src/java/com/zimbra/cs/taglib/ZJspSession.java
@@ -522,7 +522,9 @@ public class ZJspSession {
             // see if we can get a mailbox from the auth token
             ZMailbox.Options options = new ZMailbox.Options(authToken, getSoapURL(context));
             options.setNoSession(true);
-            options.setAuthAuthToken(false);
+            options.setAuthAuthToken(true);
+            // Get a csrf token too
+            options.setCsrfSupported(true);
             options.setTargetAccount(targetAccountId);
             options.setTargetAccountBy(Key.AccountBy.id);
             options.setClientIp(getRemoteAddr(context));
@@ -537,7 +539,9 @@ public class ZJspSession {
             // see if we can get a mailbox from the auth token
             ZMailbox.Options options = new ZMailbox.Options(authToken, getSoapURL(context));
             options.setNoSession(true);
-            options.setAuthAuthToken(false);
+            options.setAuthAuthToken(true);
+            // Get a csrf token too
+            options.setCsrfSupported(true);
             options.setTargetAccount(targetAccountId);
             options.setTargetAccountBy(Key.AccountBy.id);
             options.setClientIp(getRemoteAddr(context));
