603a27d022973f3bc7ec7d1be0b0be0cc4407599
Gren Elliot <gren.elliot@zimbra.com>
2015-03-09 09:15:31 -0500

    Bug:97900
    Html client uses fileAsStr string instead of fullName like the Ajax client uses when picking contacts/creating associated email address in compose window.
    This causes an issue in Japanese as the "," character from fileAsStr's "Last, First" format isn't appropriate.
    
    Decided not to make a Japanese specific change for the algorithm for generating fileAsStr as not sure what the consequences would be and also, if that was the fix, it wouldn't help with legacy data.
    https://reviewboard.eng.zimbra.com/r/4259/
    JUDASPRIEST ---> main
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=97900
    
    Copied from Perforce
     Change: 539018

--- src/java/com/zimbra/cs/taglib/bean/ZContactHitBean.java
+++ src/java/com/zimbra/cs/taglib/bean/ZContactHitBean.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -16,13 +16,14 @@
  */
 package com.zimbra.cs.taglib.bean;
 
+import com.google.common.base.Strings;
 import com.zimbra.client.ZContactHit;
 import com.zimbra.client.ZEmailAddress;
 
 public class ZContactHitBean extends ZSearchHitBean {
-    
-    private ZContactHit mHit;
-    
+
+    private final ZContactHit mHit;
+
     public ZContactHitBean(ZContactHit hit) {
         super(hit, HitType.contact);
         mHit = hit;
@@ -31,20 +32,20 @@ public class ZContactHitBean extends ZSearchHitBean {
     public String getFlags() { return mHit.getFlags(); }
 
     public boolean getHasFlags() { return mHit.hasFlags(); }
-    
+
     public boolean getIsFlagged() { return mHit.isFlagged(); }
 
     public boolean getHasTags() { return mHit.hasTags(); }
 
     public boolean getIsGroup() { return mHit.isGroup(); }
-    
-    public String getTagIds() { return mHit.getTagIds(); }    
+
+    public String getTagIds() { return mHit.getTagIds(); }
 
     public String getFolderId() { return mHit.getFolderId(); }
 
     public String getRevision() { return mHit.getRevision(); }
-    
-    public String getFileAsStr() { return mHit.getFileAsStr(); } 
+
+    public String getFileAsStr() { return mHit.getFileAsStr(); }
 
     public String getEmail() { return mHit.getEmail(); }
 
@@ -62,6 +63,7 @@ public class ZContactHitBean extends ZSearchHitBean {
 
     public String getDlist() { return mHit.getDlist(); }
 
+    @Override
     public String toString() { return mHit.toString(); }
 
     public String getFullName() { return mHit.getFullName(); }
@@ -77,28 +79,28 @@ public class ZContactHitBean extends ZSearchHitBean {
     public String getNameSuffix() { return mHit.getNameSuffix(); }
     public String getCompany() { return mHit.getCompany(); }
     public String getPhoneticCompany() { return mHit.getPhoneticCompany(); }
-    
+
     /**
      * @return time in msecs
      */
     public long getMetaDataChangedDate() { return mHit.getMetaDataChangedDate(); }
-    
+
     /**
      * @return first email from email/2/3 that is set, or an empty string
      */
     public String getDisplayEmail() {
         if (getIsGroup())
             return getDlist();
-        else if (getEmail() != null && getEmail().length() > 0) 
+        else if (getEmail() != null && getEmail().length() > 0)
             return getEmail();
-        else if (getEmail2() != null && getEmail2().length() > 0) 
+        else if (getEmail2() != null && getEmail2().length() > 0)
             return getEmail2();
-        else if (getEmail3() != null && getEmail3().length() > 0) 
+        else if (getEmail3() != null && getEmail3().length() > 0)
             return getEmail3();
         else
             return "";
     }
-    
+
     /**
      *
      * @return the "full" email address suitable for inserting into a To/Cc/Bcc header
@@ -107,7 +109,22 @@ public class ZContactHitBean extends ZSearchHitBean {
         if (getIsGroup()) {
             return getDlist();
         } else {
-            return new ZEmailAddress(getDisplayEmail(), null, getFileAsStr(), ZEmailAddress.EMAIL_TYPE_TO).getFullAddress();
+            /* Used to use FileAsStr but Bug 97900 suggests the default "Last, First" form wasn't appropriate for
+             * Japanese - where the "," should be omitted. We actually use that form in FullName (and the Ajax
+             * client currently uses that).  Also:
+             *     http://www.slipstick.com/outlook/contacts/bulk-change-outlook-contacts-email-display-name-format/
+             * has this to say:
+             *     The default email display name in the address book used to be "Full Name (Email1)",
+             *     "Full Name (Email2)", "Full Name (Email3)" but beginning with Outlook 2002,
+             *     it was changed to "Full Name (email address)"
+             * all of which implies that FullName is more appropriate if Outlook compatibility is desired.
+             */
+            String commentPart = getFullName();
+            if (Strings.isNullOrEmpty(commentPart)) {
+                commentPart = Strings.nullToEmpty(getFileAsStr());
+            }
+            return new ZEmailAddress(getDisplayEmail(), null, commentPart,
+                            ZEmailAddress.EMAIL_TYPE_TO).getFullAddress();
         }
     }
 
