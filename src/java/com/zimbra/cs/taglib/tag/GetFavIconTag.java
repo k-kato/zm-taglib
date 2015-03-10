13be31919c8874e3defa8634719fd72ea7b0aebf
Komal Kakani <kkakani@zimbra.com>
2015-03-10 06:43:41 -0500

    Merging using JUDASPRIEST_to_main Change 539123 by kkakani@kkakani_new on 2015/03/10 06:30:24
    bug:97906 - #(sharp) and ;(semi-colon) in the name of an uploaded 
    file is not escaped on Standard HTML client
    -Use encodePath() method from HttpUtil.java, it URL-encodes the 
    given path.
    -Fixed the issue for both standard and mobile HTML client
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=97906
    
    Copied from Perforce
     Change: 539127

--- src/java/com/zimbra/cs/taglib/tag/GetFavIconTag.java
+++ src/java/com/zimbra/cs/taglib/tag/GetFavIconTag.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2009, 2010, 2011, 2013, 2014 Zimbra, Inc.
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -16,20 +16,19 @@
  */
 package com.zimbra.cs.taglib.tag;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.PageContext;
-
+import com.zimbra.cs.account.soap.SoapProvisioning;
+import com.zimbra.cs.account.Entry;
 import com.zimbra.common.account.Key;
 import com.zimbra.common.localconfig.LC;
 import com.zimbra.common.soap.AdminConstants;
-import com.zimbra.common.util.HttpUtil;
 import com.zimbra.common.util.ZimbraLog;
-import com.zimbra.cs.account.Entry;
-import com.zimbra.cs.account.Provisioning;
-import com.zimbra.cs.account.soap.SoapProvisioning;
+import com.zimbra.common.util.HttpUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspContext;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+import java.io.IOException;
 
 public class GetFavIconTag extends ZimbraSimpleTag {
 
@@ -56,12 +55,11 @@ public class GetFavIconTag extends ZimbraSimpleTag {
 
 	// simple tag methods
 
-	@Override
-    public void doTag() throws JspException, IOException {
+	public void doTag() throws JspException, IOException {
 		try {
 			// get provisioning
 			String soapUri =
-			    Provisioning.getInstance().getLocalServer().getAdminServiceScheme() +
+				LC.zimbra_admin_service_scheme.value() +
 				LC.zimbra_zmprov_default_soap_server.value() +
 				':' +
 				LC.zimbra_admin_service_port.intValue() +
