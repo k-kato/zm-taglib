0b67dd177269bb0350539eecfa5050eff05b545e
Komal Kakani <kkakani@zimbra.com>
2015-06-25 04:57:48 -0500

    Merging using JUDASPRIEST_to_main Change 548714 by kkakani@kkakani_new on 2015/06/25 04:00:44
    bug:99917 - login page changes to deal with change in AuthResponse 
    (no SOAP fault) when two-factor auth is enabled
    -Login page changes to deal with the changes in AuthResponse in case 
    of two factor auth.
    -Toggle the HTML rendering on the login page based on two factor 
    required boolean. 
    -Since the server now sends a temporary auth token, ensure that the 
    final auth token is re-written in the ZM_AUTH_TOKEN cookie.
    -Changes to GetValidLocale tag, pass the auth token as one of the 
    params instead of retrieving it from the request object.
    -Stay on the one-time code string in case the user enters an 
    incorrect totp code since we do not need the password to be sent 
    with the totp code now. 
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=99917
    
    Copied from Perforce
     Change: 548722

--- src/java/com/zimbra/cs/taglib/memcached/RouteCache.java
+++ src/java/com/zimbra/cs/taglib/memcached/RouteCache.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -18,43 +18,15 @@
 package com.zimbra.cs.taglib.memcached;
 
 import com.zimbra.common.service.ServiceException;
-import com.zimbra.common.util.memcached.MemcachedMap;
-import com.zimbra.common.util.memcached.MemcachedSerializer;
-import com.zimbra.common.util.memcached.ZimbraMemcachedClient;
-import com.zimbra.cs.taglib.ZJspSession;
 
-public class RouteCache {
+public interface RouteCache {
+    /**
+     * @param scheme "http" or "https"
+     */
+    public String get(String accountId, String scheme) throws ServiceException;
 
-    private static RouteCache sTheInstance = new RouteCache();
-    private MemcachedMap<RouteCacheKey, String> mMemcachedLookup;
-
-    public static RouteCache getInstance() { return sTheInstance; }
-
-    RouteCache() {
-        ZimbraMemcachedClient memcachedClient = MemcachedConnector.getClient();
-        RouteSerializer serializer = new RouteSerializer();
-        mMemcachedLookup = new MemcachedMap<RouteCacheKey, String>(memcachedClient, serializer, false);
-    }
-
-    private static class RouteSerializer implements MemcachedSerializer<String> {
-        @Override
-        public Object serialize(String value) {
-            return value;
-        }
-
-        @Override
-        public String deserialize(Object obj) throws ServiceException {
-            return (String) obj;
-        }
-    }
-
-    public String get(String accountId) throws ServiceException {
-        RouteCacheKey key = new RouteCacheKey(ZJspSession.isProtocolModeHttps() ?  "https" : "http", accountId);
-        return mMemcachedLookup.get(key);
-    }
-
-    public void put(String accountId, String route) throws ServiceException {
-        RouteCacheKey key = new RouteCacheKey(ZJspSession.isProtocolModeHttps() ?  "https" : "http", accountId);
-        mMemcachedLookup.put(key, route);
-    }
+    /**
+     * @param scheme "http" or "https"
+     */
+    public void put(String accountId, String scheme, String route) throws ServiceException;
 }
