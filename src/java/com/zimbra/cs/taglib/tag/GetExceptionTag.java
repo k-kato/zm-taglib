da95b0388cfe2e02685edae2841a55ecc93a5284
Unknown Perforce User <jflanigan>
2015-07-30 16:49:08 -0500

    bug: none
    rollback change 551664; submitted accidentally
    
    Copied from Perforce
     Change: 551667

--- src/java/com/zimbra/cs/taglib/tag/GetExceptionTag.java
+++ src/java/com/zimbra/cs/taglib/tag/GetExceptionTag.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2013, 2014 Zimbra, Inc.
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -22,24 +22,22 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.SkipPageException;
 
-import org.eclipse.jetty.io.RuntimeIOException;
-
-import com.zimbra.common.service.ServiceException;
-import com.zimbra.common.util.ZimbraLog;
-import com.zimbra.common.zclient.ZClientException;
 import com.zimbra.cs.taglib.bean.ZExceptionBean;
 import com.zimbra.cs.taglib.bean.ZTagLibException;
+import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.common.zclient.ZClientException;
+import com.zimbra.common.service.ServiceException;
+import org.eclipse.jetty.io.RuntimeIOException;
 
 public class GetExceptionTag extends ZimbraSimpleTag {
-
+    
     private String mVar;
     private Exception mException;
-
+    
     public void setVar(String var) { this.mVar = var; }
-
+    
     public void setException(Exception e) { this.mException = e; }
-
-    @Override
+    
     public void doTag() throws JspException, IOException {
         ZExceptionBean eb = new ZExceptionBean(mException);
         Exception e = eb.getException();
@@ -50,6 +48,5 @@ public class GetExceptionTag extends ZimbraSimpleTag {
                 ZimbraLog.webclient.warn("local exception", e);
         }
         getJspContext().setAttribute(mVar, eb,  PageContext.PAGE_SCOPE);
-        getJspContext().setAttribute(mVar, eb,  PageContext.SESSION_SCOPE);
     }
 }
