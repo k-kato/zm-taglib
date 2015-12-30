dfb18036dc193534e724d331ab213446857e5bb0
Snehal Devasthali <sdevasthali@zimbra.com>
2015-12-29 23:42:21 -0600

    Bug:102684 - Composed message is not sent and it is lost sometimes
    Back out changelist 561996
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=102684
    
    Copied from Perforce
     Change: 562233

--- src/java/com/zimbra/cs/taglib/bean/ZComposeUploaderBean.java
+++ src/java/com/zimbra/cs/taglib/bean/ZComposeUploaderBean.java
@@ -333,6 +333,7 @@ public class ZComposeUploaderBean {
         compose.setInReplyTo(getParam(F_inreplyto));
         compose.setMessageId(getParam(F_messageid));
         compose.setDraftId(getParam(F_draftid));
+        compose.setSendUID(getParam(F_sendUID));
 
         compose.setApptFolderId(getParam(F_apptFolderId));
         compose.setInviteReplyVerb(getParam(F_inviteReplyVerb));
