141848f3b69f552a5eb32a2e4d1d5608d4d747cc
Vishal Mahajan <vmahajan@zimbra.com>
2016-06-23 07:28:01 -0500

    bug: 101023 - zimbraHelpAdvancedURL, zimbraHelpStandardURL, zimbraHelpAdminURL and zimbraHelpDelegatedURL does not work
    This change is a part of the overall fix required for bug 101023.
    
    - Enabled passing csrf token to SoapProvisioning
    - Enabled GetDomainInfoTag to make authenticated requests
    - GetDomainInfo request handler shouldn't be mandating admin auth, given that the attributes flagged with "domainInfo" really need to be available to end user applications
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=101023
    
    Copied from Perforce
     Change: 572877

--- src/java/com/zimbra/cs/taglib/tag/GetDomainInfoTag.java
+++ src/java/com/zimbra/cs/taglib/tag/GetDomainInfoTag.java
@@ -17,12 +17,13 @@
 package com.zimbra.cs.taglib.tag;
 
 import com.zimbra.common.account.Key;
-import com.zimbra.common.account.Key.DomainBy;
+import com.zimbra.common.auth.ZAuthToken;
 import com.zimbra.common.localconfig.LC;
 import com.zimbra.common.net.SocketFactories;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.AdminConstants;
 import com.zimbra.common.util.DateUtil;
+import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.account.Domain;
 import com.zimbra.cs.account.soap.SoapProvisioning;
 
@@ -44,12 +45,16 @@ public class GetDomainInfoTag extends ZimbraSimpleTag {
     private String mVar;
     private Key.DomainBy mBy;
     private String mValue;
+    private String mCsrfToken;
+    private ZAuthToken mAuthToken;
 
     private static final Map<String, CachedDomain> mCache = new HashMap<String, CachedDomain>();
 
     public void setVar(String var) { this.mVar = var; }
     public void setBy(String by) throws ServiceException { this.mBy = Key.DomainBy.fromString(by); }
     public void setValue(String value) { this.mValue = value; }
+    public void setAuthtoken(ZAuthToken authToken) { this.mAuthToken = authToken; }
+    public void setCsrftoken(String csrfToken) { this.mCsrfToken = csrfToken; }
 
     private static final String DEFAULT_TTL_STR = "60m";
     private static final long DEFAULT_TTL = 60*60*1000;
@@ -68,14 +73,18 @@ public class GetDomainInfoTag extends ZimbraSimpleTag {
     private String getCacheKey() { return mBy +"/" + mValue; }
 
     private Domain checkCache() {
-        CachedDomain cd = mCache.get(getCacheKey());
-        if (cd != null) {
-            if (cd.expireTime > System.currentTimeMillis())
-                return cd.domain;
+        if (mAuthToken == null) {
+            CachedDomain cd = mCache.get(getCacheKey());
+            if (cd != null) {
+                if (cd.expireTime > System.currentTimeMillis())
+                    return cd.domain;
+            }
         }
         Domain d = getInfo();
-        synchronized(mCache) {
-            mCache.put(getCacheKey(), new CachedDomain(d));
+        if (mAuthToken == null) {
+            synchronized(mCache) {
+                mCache.put(getCacheKey(), new CachedDomain(d));
+            }
         }
         return d;
     }
@@ -85,10 +94,12 @@ public class GetDomainInfoTag extends ZimbraSimpleTag {
         String mServer = LC.zimbra_zmprov_default_soap_server.value();
         int mPort = LC.zimbra_admin_service_port.intValue();
         sp.soapSetURI(LC.zimbra_admin_service_scheme.value()+mServer+":"+mPort+ AdminConstants.ADMIN_SERVICE_URI);
+        sp.setAuthToken(mAuthToken);
+        sp.setCsrfToken(mCsrfToken);
         try {
             return sp.getDomainInfo(mBy, mValue);
         } catch (ServiceException e) {
-            e.printStackTrace();
+            ZimbraLog.webclient.error("Error during GetDomainInfoRequest invocation", e);
             return null;
         }
     }
