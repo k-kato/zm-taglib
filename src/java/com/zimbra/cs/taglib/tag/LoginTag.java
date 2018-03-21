569ff8113c01c589e574f4427548f4899ff2d2f3
nilesh pawar <nilesh.pawar@synacor.com>
2018-03-21 13:56:35 +0000

    NICPS-13: Add logic to login to display Captcha module after 2 failed attempts of login

--- src/java/com/zimbra/cs/taglib/tag/LoginTag.java
+++ src/java/com/zimbra/cs/taglib/tag/LoginTag.java
@@ -17,8 +17,13 @@
 package com.zimbra.cs.taglib.tag;
 
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Map;
+import java.net.URL;
+import java.nio.charset.Charset;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -39,6 +44,10 @@ import com.zimbra.common.util.ngxlookup.NginxAuthServer;
 import com.zimbra.cs.account.AccountServiceException;
 import com.zimbra.cs.taglib.ZJspSession;
 import com.zimbra.cs.taglib.ngxlookup.NginxRouteLookUpConnector;
+import com.zimbra.cs.account.AccountServiceException;
+import com.zimbra.cs.account.AccountServiceException.AuthFailedServiceException;
+
+import org.json.JSONObject;
 
 public class LoginTag extends ZimbraSimpleTag {
 
@@ -128,6 +137,10 @@ public class LoginTag extends ZimbraSimpleTag {
         try {
             PageContext pageContext = (PageContext) jctxt;
             HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
+
+            if(request.getParameter("g-recaptcha-response")!= null && !isCaptchaValid("6LdfLE0UAAAAAC3rf4G4oepXp7H0AEEegdfeDvUA", request.getParameter("g-recaptcha-response"))){
+                throw AuthFailedServiceException.INVALID_CAPTCHA();
+            }
 
             String serverName = request.getServerName();
 
@@ -272,6 +285,29 @@ public class LoginTag extends ZimbraSimpleTag {
             ZimbraCookie.addHttpOnlyCookie(response, name, trustedToken, path, secondsLeft, secure);
         } else {
             ZimbraCookie.clearCookie(response, name);
+        }
+    }
+
+    public static boolean isCaptchaValid(String secretKey, String response) {
+        try {
+            String url = "https://www.google.com/recaptcha/api/siteverify?"
+                    + "secret=" + secretKey
+                    + "&response=" + response;
+            InputStream res = new URL(url).openStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(res, Charset.forName("UTF-8")));
+
+            StringBuilder sb = new StringBuilder();
+            int cp;
+            while ((cp = rd.read()) != -1) {
+                sb.append((char) cp);
+            }
+            String jsonText = sb.toString();
+            res.close();
+
+            JSONObject json = new JSONObject(jsonText);
+            return json.getBoolean("success");
+        } catch (Exception e) {
+            return false;
         }
     }
 }
