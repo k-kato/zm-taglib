46c311607ea1be4d2510451a9504bd4fdf73cdd2
Koichi Kato <koichi.kato@synacor.com>
2018-03-23 10:48:28 +0900

    NICPS-32: fixing as per review comment

--- src/java/com/zimbra/cs/taglib/bean/ZMessageComposeBean.java
+++ src/java/com/zimbra/cs/taglib/bean/ZMessageComposeBean.java
@@ -552,8 +552,7 @@ public class ZMessageComposeBean {
             color = Integer.parseInt(mColor);
             Color colorObj = Color.fromInt(color);
             return colorObj.getName();
-        } catch (NumberFormatException e) {
-        } catch (ServiceException e) {
+        } catch (NumberFormatException | ServiceException e) {
         }
         return null;
     }
